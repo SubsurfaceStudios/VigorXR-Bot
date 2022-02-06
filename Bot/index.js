d84e34f5526a97dd9db6ca1b138c815a6a752459
bubby932 <tvrec10@gmail.com>
2022-02-05 23:15:37 -0500

    89nv54

--- Bot/index.js
+++ Bot/index.js
@@ -1,4 +1,5 @@
 const { Client, Collection, MessageEmbed } = require("discord.js");
+const config = require('./config.json');
 
 const client = new Client({
     intents: 32767,
@@ -8,9 +9,8 @@ module.exports = client.application;
 // Initializing the project
 require("./handler")(client.application);
 
-client.login(client.application.config.token);
+client.login(config.token);
 
 // Global Variables
 client.application.commands = new Collection();
 client.application.slashCommands = new Collection();
-client.application.config = require("./config.json");
