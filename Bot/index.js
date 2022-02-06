46ad6c2c0c337fd5714d39ff7a47043ae732db0d
bubby932 <tvrec10@gmail.com>
2022-02-05 23:13:47 -0500

    hgjf

--- Bot/index.js
+++ Bot/index.js
@@ -5,14 +5,12 @@ const client = new Client({
 });
 module.exports = client.application;
 
-// Global Variables
-client.application.commands = new Collection();
-client.application.slashCommands = new Collection();
-client.application.config = require("./config.json");
-
-
-
 // Initializing the project
 require("./handler")(client.application);
 
 client.application.login(client.application.config.token);
+
+// Global Variables
+client.application.commands = new Collection();
+client.application.slashCommands = new Collection();
+client.application.config = require("./config.json");
